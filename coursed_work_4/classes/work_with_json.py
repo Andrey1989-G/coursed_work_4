0f61528bc79c87bcb82b0a5d316e6ef53beab704
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-10-01 11:20:51 +0500

    gap 3

--- coursed_work_4/classes/work_with_json.py
+++ coursed_work_4/classes/work_with_json.py
@@ -1,12 +1,19 @@
 import json
 import os
 
-from classes.abstract_class import JSONSaver
+from classes.abstract_class import JSONSaver, GetInfo
+from classes.input_error import Input_error
 
 
 class WorkWithJson(JSONSaver):
     """класс для работы с json файлом"""
     file_path = 'data/vacancy.json'
+
+    def get_vacancies(self):
+        """получение информации из файла"""
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            s = json.load(file)
+            return s
 
     def add_vacancy(self, id_vacancy):
         """добавление по id в json файл"""
@@ -49,3 +56,68 @@ class WorkWithJson(JSONSaver):
                             salary.append(i["Заработная плата:"])
                 return salary
 
+# class Vacancy_local(WorkWithJson,Input_error):
+#     """методы работы с вакансиями"""
+#
+#     def __init__(self, vacancy_id=None, vacancy_name=None, vacancy_url=None,
+#                  vacancy_salary=None, vacancy_city=None, vacancy_requirement=None,
+#                  vacancy_responsibility=None):
+#         try:
+#             self.vacancy_id = vacancy_id
+#             self.vacancy_name = vacancy_name
+#             self.vacancy_url = vacancy_url
+#             self.vacancy_salary = vacancy_salary
+#             self.vacancy_city = vacancy_city
+#             self.vacancy_requirement = vacancy_requirement
+#             self.vacancy_responsibility = vacancy_responsibility
+#         except Input_error as s:
+#             print(s.message)
+#
+#     def sorted_vacancy(self):
+#         """Метод сортировки информации из api"""
+#         data = self.get_vacancies(self.keyword)
+#         sorted_vacancy = []
+#         for item in data['items']:
+#             salary = item['salary']
+#             salary_range = f"{salary['from']}-{salary['to']}, валюта {salary['currency']}" if salary and salary[
+#                 'from'] and salary['to'] else "Не указано"
+#             vacancy = {
+#                 "id вакансии": int(item['id']),
+#                 "Название": item['name'],
+#                 "Заработная плата": salary_range,
+#                 "Город": item["area"]["name"],
+#                 "Требование": item['snippet']['requirement'],
+#                 "Обязанности": item['snippet']['responsibility'],
+#                 "cсылка": item['url']
+#             }
+#             sorted_vacancy.append(vacancy)
+#         return sorted_vacancy
+#
+#     def sort_by(self, info, per_page):
+#         """сортировка вакансий"""
+#         self.per_page = per_page
+#         self.info = info
+#         sorted_vacancy = self.sorted_vacancy()
+#         if sorted_vacancy:
+#             sorted_by_city = []
+#             for i in sorted_vacancy:
+#                 if self.info is not None and self.info in i['Город']:
+#                     sorted_by_city.append(i)
+#                 else:
+#                     sorted_vacancy.sort(key=lambda i: i.get(self.info, ''), reverse=True)
+#             else:
+#                 return sorted_vacancy[:self.per_page]
+#         else:
+#             raise Input_error
+#
+#     def to_dict(self):
+#         """переводим в словарь"""
+#         return {
+#             "id вакансии:": self.vacancy_id,
+#             "Название:": self.vacancy_name,
+#             "cсылка:": self.vacancy_url,
+#             "Заработная плата:": self.vacancy_salary,
+#             "Город:": self.vacancy_city,
+#             "Требование:": self.vacancy_requirement,
+#             "Обязанности:": self.vacancy_responsibility
+#         }
--- coursed_work_4/function.py
+++ coursed_work_4/function.py
@@ -7,11 +7,12 @@ from classes.input_error import Input_error
 
 def start_with_user():
     """Начало работы, приветствие и выбор сайта для поиска"""
-    site = ["HeadHunter", "SuperJob"]
+    site = ["HeadHunter", "SuperJob", "Местная БД"]
     choоse_platforms = input("Здравствуйте, выберите сайт для поиска вакансий:\n"
                             f"1- {site[0]}\n"
                             f"2- {site[1]}\n"
-                            "3- Выход\n")
+                            f"3- {site[2]}\n"
+                            "4- Выход\n")
     if choоse_platforms == "1":
         print(f"Выбранная платформа - {site[0]}")
         vacancies_from_hh = Vacancy()
@@ -21,6 +22,10 @@ def start_with_user():
         superjob_api = VacancySuperJob()
         return superjob_api
     elif choоse_platforms == "3":
+        print(f"Выбранная платформа - {site[2]}")
+        file_json = WorkWithJson()
+        return file_json.get_vacancies()
+    elif choоse_platforms == "4":
         print("До свидания")
         return None
     else:
@@ -57,77 +62,6 @@ def find_vacancy():
             raise ValueError
     else:
         return None
-
-
-def add_vacancy(vacancies):
-    """Добавление вакансии в файл"""
-    from_vacancies = WorkWithJson()
-    if vacancies is not None:
-        add_vacancy = int(input(
-            "Для добавления вакансии в файл введите 'id вакансии': "))
-        vacancy_add_list = []
-        if isinstance(add_vacancy, int):
-            for i in vacancies:
-                if i['id вакансии'] == add_vacancy:
-                    vacancy_add = Vacancy(vacancy_id=i['id вакансии'],
-                                          vacancy_name=i['Название'],
-                                          vacancy_url=i['cсылка'],
-                                          vacancy_salary=i['Заработная плата'],
-                                          vacancy_city=i['Город'],
-                                          vacancy_requirement=i['Требование'],
-                                          vacancy_responsibility=i['Обязанности']
-                                          )
-                    vacancy_add_list.append(vacancy_add.to_dict())
-            from_vacancies.add_vacancy(vacancy_add_list)
-            print("Вакансия добавлена в файл!")
-        else:
-            raise ValueError
-
-
-def del_vacancy():
-    """Удаление вакансий из файла"""
-    vacancies_from = WorkWithJson()
-    delete_vacancy = int(input(
-        "Для удаления вакансии из файла,укажите 'id вакансии': "))
-    if isinstance(delete_vacancy, int):
-        result = vacancies_from.delete_vacancy(delete_vacancy)
-        return result
-    else:
-        raise ValueError
-
-
-def full_info():
-    """Получение информации о вакансии из файла"""
-    vacancies_from = WorkWithJson()
-    id_vacancy = int(input("id вакансии: "))
-    if isinstance(id_vacancy, int):
-        print(f"Информация о вакансии с id {id_vacancy}: ")
-        vacancies_from.get_vacancies_by(id_vacancy)
-    else:
-        raise ValueError
-
-
-def comparison_by_salary():
-    """Сравнение двух добавленных в json вакансий по заработной плате"""
-    vacancies_from = WorkWithJson()
-    id_vac_x = int(input("Вакансия №1: "))
-    id_vac_y = int(input("Вакансия №2: "))
-    if isinstance(id_vac_x, int) and isinstance(id_vac_y, int):
-        salary = vacancies_from.get_vacancies_by(id_vac_x, id_vac_y)
-        vacancies_from.vacancy_salary = int(
-            salary[0].split('-')[0]) if salary[0].split('-')[0].isdigit() else 0
-        vacancies_from.other_salary = int(
-            salary[1].split('-')[0]) if salary[1].split('-')[0].isdigit() else 0
-
-        if vacancies_from.vacancy_salary >= vacancies_from.other_salary:
-            print(f"Вакансия {id_vac_x} c заработной платой {vacancies_from.vacancy_salary},\n"
-                  f"больше или равна вакансии {id_vac_y} c заработной платой {vacancies_from.other_salary}")
-        else:
-            print(f"Вакансия {id_vac_y} c заработной платой {vacancies_from.vacancy_salary},\n"
-                  f"меньше или равна вакансии {id_vac_x} c заработной платой {vacancies_from.other_salary}")
-    else:
-        raise ValueError
-
 
 def main():
     """Вывод найденных вакансий и дальнейшие действия с ними"""
@@ -170,3 +104,76 @@ def main():
             return None
     except (Input_error, ValueError) as error:
         print(error.message)
+
+# s = WorkWithJson()
+# print(s.get_vacancy())
+
+def add_vacancy(vacancies):
+    """Добавление вакансии в файл"""
+    from_vacancies = WorkWithJson()
+    if vacancies is not None:
+        add_vacancy = int(input(
+            "Для добавления вакансии в файл введите 'id вакансии': "))
+        vacancy_add_list = []
+        if isinstance(add_vacancy, int):
+            for i in vacancies:
+                if i['id вакансии'] == add_vacancy:
+                    vacancy_add = Vacancy(vacancy_id=i['id вакансии'],
+                                          vacancy_name=i['Название'],
+                                          vacancy_url=i['cсылка'],
+                                          vacancy_salary=i['Заработная плата'],
+                                          vacancy_city=i['Город'],
+                                          vacancy_requirement=i['Требование'],
+                                          vacancy_responsibility=i['Обязанности']
+                                          )
+                    vacancy_add_list.append(vacancy_add.to_dict())
+            from_vacancies.add_vacancy(vacancy_add_list)
+            print("Вакансия добавлена в файл!")
+        else:
+            raise ValueError
+
+
+def del_vacancy():
+    """Удаление вакансий из файла"""
+    vacancies_from = WorkWithJson()
+    delete_vacancy = int(input(
+        "Для удаления вакансии из файла,укажите 'id вакансии': "))
+    if isinstance(delete_vacancy, int):
+        result = vacancies_from.delete_vacancy(delete_vacancy)
+        return result
+    else:
+        raise ValueError
+
+
+def full_info():
+    """Получение информации о вакансии из файла"""
+    vacancies_from = WorkWithJson()
+    print(f"Список вакансий в местной БД:\n"
+          f"{vacancies_from.get_vacancy()}")
+    id_vacancy = int(input("id вакансии: "))
+    if isinstance(id_vacancy, int):
+        print(f"Информация о вакансии с id {id_vacancy}: ")
+        vacancies_from.get_vacancies_by(id_vacancy)
+    else:
+        raise ValueError
+
+def comparison_by_salary():
+    """Сравнение двух добавленных в json вакансий по заработной плате"""
+    vacancies_from = WorkWithJson()
+    id_vac_x = int(input("Вакансия №1: "))
+    id_vac_y = int(input("Вакансия №2: "))
+    if isinstance(id_vac_x, int) and isinstance(id_vac_y, int):
+        salary = vacancies_from.get_vacancies_by(id_vac_x, id_vac_y)
+        vacancies_from.vacancy_salary = int(
+            salary[0].split('-')[0]) if salary[0].split('-')[0].isdigit() else 0
+        vacancies_from.other_salary = int(
+            salary[1].split('-')[0]) if salary[1].split('-')[0].isdigit() else 0
+
+        if vacancies_from.vacancy_salary >= vacancies_from.other_salary:
+            print(f"Вакансия {id_vac_x} c заработной платой {vacancies_from.vacancy_salary},\n"
+                  f"больше или равна вакансии {id_vac_y} c заработной платой {vacancies_from.other_salary}")
+        else:
+            print(f"Вакансия {id_vac_y} c заработной платой {vacancies_from.vacancy_salary},\n"
+                  f"меньше или равна вакансии {id_vac_x} c заработной платой {vacancies_from.other_salary}")
+    else:
+        raise ValueError
